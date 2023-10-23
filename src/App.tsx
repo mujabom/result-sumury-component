8e26e72ae707dc72baed7295f3e750fa0fc756ca
mujabom <msaabwsr@gmail.com>
2023-10-23 20:43:33 +0300

    updated hover effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,89 @@
-
-
-export default function ScoreWidget(){
-
-return (
-  <div className="bg-gradient-to-b flex sm:w-auto md:w-1/2 flex-col justify-around items-center py-3 px-9 from-light-slate-blue rounded-3xl to-light-royal-blue">
-  <h4 className="text-light-lavender font-bold">Your Result</h4>
-  <div className="rounded-full  aspect-square flex flex-col justify-center h-36 bg-gradient-to-b from-violet-blue to-persian-blue ">
-    <div className="flex flex-row justify-center font-extrabold text-white items-center text-5xl">
-      76
-    </div>
-    <p className="flex justify-center text-light-lavender items-center text-xs">
-      of 100
-    </p>
-  </div>
-  <h2 className="text-2xl text-white font-bold">Great</h2>
-  <p className="flex justify-center text-light-lavender text-center items-center text-xs">
-    You scored higher than 65% of the people who have taken these tests.
-  </p>
-</div>
-);
-
+import ScoreWidget from "./ScoreWidget";
+import iconmemory from "./asset/images/icon-memory.svg";
+import iconnreaction from "./asset/images/icon-reaction.svg";
+import iconverbal from "./asset/images/icon-verbal.svg";
+import iconvisual from "./asset/images/icon-visual.svg";
+import data from "./data.json";
+type Data = {
+  category: "Memory" | "Reaction" | "Verbal" | "Visual";
+  icon: string;
+  score: number;
 };
+export default function App5() {
+  return (
+    <div className="md:min-h-screen bg-pale-blue justify-center flex align-middle items-center">
+
+      <div className="flex rounded-3xl md:w-[32rem] bg-white shadow-lg flex-col md:flex-row  ">
+        <ScoreWidget />
+        <div className="px-9 py-6 flex flex-col gap-3 md:w-1/2">
+          <h4 className="font-bold text-dark-gray-blue ">Summary</h4>
+          <div className="flex flex-col gap-3">
+          {data.map((item) => {
+            const data = item as Data;
+            return (
+              <InfoBar
+              key={data.category}
+                field={data.category}
+                score={data.score}
+              />
+            );
+          })}</div>
+          <button className=" rounded-3xl py-2 text-sm text-white justify-end
+            hover:bg-gradient-to-b from-light-slate-blue to-light-royal-blue
+          bg-dark-gray-blue font-bold w-full">
+            Continue
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+type InfoBarProps = {
+  field: "Memory" | "Reaction" | "Verbal" | "Visual";
+  score: number;
+};
+
+function InfoBar({ field, score }: InfoBarProps) {
+  const colors = {
+    Memory: "bg-orangey-yellow",
+    Reaction: "bg-light-red",
+    Verbal: "bg-green-teal",
+    Visual: "bg-cobalt-blue",
+  };
+  function getIcon(field: string) {
+    switch (field) {
+      case "Memory":
+        return iconmemory;
+      case "Reaction":
+        return iconnreaction;
+      case "Verbal":
+        return iconverbal;
+      case "Visual":
+        return iconvisual;
+      default:
+        return iconmemory;
+    }
+  }
+
+  const textColors = {
+    Memory: "text-orangey-yellow",
+    Reaction: "text-light-red",
+    Verbal: "text-green-teal",
+    Visual: "text-cobalt-blue",
+  };
+  return (
+    <div
+      className={`flex flex-row ${colors[field]} bg-opacity-10 w-full p-2 justify-between rounded-lg`}
+    >
+      <div className="flex flex-row gap-2">
+        <img src={getIcon(field)} alt="memory icon" className="h-5" />
+        <p className={`${textColors[field]} text-sm`}>{field}</p>
+      </div>
+      <div className="flex flex-row gap-1">
+        <p className="text-dark-gray-blue">{score}</p>
+        <p className="text-light-lavender">/100</p>
+      </div>
+    </div>
+  );
+}
