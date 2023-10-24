e23c47c8006125225d0fb0253ede96993749e305
mujabom <msaabwsr@gmail.com>
2023-10-25 01:51:22 +0300

    container basic css

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Sumury from "./components/Sumury";
 
 export default function App5() {
   return (
-    <div>
-      <div>
+    <div className="min-h-screen grid place-items-center">
+      <div className="grid grid-cols-2 place-content-center">
         <ScoreWidget />
         <Sumury />
       </div>
--- src/components/ScoreWidget.tsx
+++ src/components/ScoreWidget.tsx
@@ -1,21 +1,20 @@
-export default function ScoreWidget(){
-
+export default function ScoreWidget() {
   return (
-    <div>
+    <div
+      className="text-light-lavender bg-gradient-to-b from-light-slate-blue to-light-royal-blue
+    grid place-items-center py-3 px-5"
+    >
       <h4>Your Result</h4>
       <div>
-        <div>
-          76
-        </div>
+        <div>76</div>
+        <p>of 100</p>
+      </div>
+      <div className="">
+        <h2>Great</h2>
         <p>
-          of 100
+          You scored higher than 65% of the people who have taken these tests.
         </p>
       </div>
-      <h2>Great</h2>
-      <p>
-        You scored higher than 65% of the people who have taken these tests.
-      </p>
     </div>
   );
-  
-  };
+}
